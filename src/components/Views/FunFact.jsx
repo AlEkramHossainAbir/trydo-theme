6b43f587648e3673bfa1a08a29fa74e21c3a758c
AlEkramHossainAbir <alabir65@gmail.com>
2022-10-29 22:52:55 +0600

    fun fact

--- src/components/Views/FunFact.jsx
+++ src/components/Views/FunFact.jsx
@@ -5,30 +5,30 @@ const FunFact = () =>{
         
         <div className="fun-fact">
         <h1 className="fun-fact-text">Our Fun Facts</h1>
-        <div className="row  text-center pb-5 container mx-auto">
+        <div className="fun-fact-count-container">
           <div className="col-lg-4 col-md-6 col-sm-4 pb-4">
-            <h1 className="text-danger fw-bold pb-3">
+            <h1 className="number-text">
               199<span className="temp-position">+</span>
             </h1>
-            <p className="text-secondary h5">
+            <p className="text-secondary">
               The standard chunk of Lorem Ipsum used since the 1500s is
               reproduced below for those.
             </p>
           </div>
           <div className="col-lg-4 col-md-6 col-sm-4 pb-4">
-            <h1 className="text-danger pb-3 fw-bold">
+            <h1 className="number-text">
               575<span className="temp-position">+</span>
             </h1>
-            <p className="text-secondary h5">
+            <p className="text-secondary">
               The standard chunk of Lorem Ipsum used since the 1500s is
               reproduced below for those.
             </p>
           </div>
           <div className="col-lg-4 col-md-6 col-sm-4 pb-4">
-            <h1 className="text-danger pb-3 fw-bold">
+            <h1 className="number-text">
               69<span className="temp-position">+</span>
             </h1>
-            <p className="text-secondary h5">
+            <p className="text-secondary">
               The standard chunk of Lorem Ipsum used since the 1500s is
               reproduced below for those.
             </p>
